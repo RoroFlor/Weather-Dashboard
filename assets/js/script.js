c94f9e8835f72efc2e6a1991fccbc7b35e9c1901
Humberto Alfaro <humbertoalfaro@Humbertos-MBP.lan1>
2023-03-25 13:40:36 -0400

    created functioning citysearchinput for users

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
-var weatherBase = "api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid={9d43e04bc13323ad8e1c9572b90948ce}"
+var weatherBase = "https://api.openweathermap.org/data/2.5/forecast?"
+var apiKey = '9d43e04bc13323ad8e1c9572b90948ce'
+var searchBtn = document.getElementById('searchBtn')
+var citySearchInput = document.querySelector('.citySearchInput')
+// var cityName = document.getElementById('cityName')
 
-fetch(weatherBase)
-.then(function(reponse){
-    return reponse.json()
-})
-.then(function(data){
-    console.log(data)
-});
+function getApi() {
+    var city = citySearchInput.value.trim()
+    if(city === "") {
+        return;
+    }
+
+    var apiUrl = weatherBase + 'q=' + city + "&units=imperial&appid=" + apiKey
+    fetch(apiUrl)
+        .then(function (reponse) {
+            return reponse.json()
+        })
+        .then(function (data) {
+            console.log(data)
+            
+
+        });
+}
+
+searchBtn.addEventListener('click', getApi)

adbdfa1d1ce09a1234ae6b63dc9072cf9f51fb5c
Humberto Alfaro <humbertoalfaro@Humbertos-MBP.lan1>
2023-04-01 15:36:53 -0400

    created forecast for the next 5 days

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@ var apiKey = '9d43e04bc13323ad8e1c9572b90948ce'
 var searchBtn = document.getElementById('searchBtn')
 var citySearchInput = document.querySelector('.citySearchInput')
 var tempWindHumidity = document.getElementById('tempWindHumidity')
+var cities = JSON.parse(localStorage.getItem('cities')) || []
 
-function getApi() {
+function getApi(city) {
 
-    var city = citySearchInput.value.trim()
 
     if (city === "") {
         return;
@@ -45,20 +45,69 @@ function getApi() {
 
     function futureForecast() {
         var fCast = weatherBase + 'forecast?' + 'q=' + city + "&units=imperial&appid=" + apiKey
-        
-        fetch(fCast)
-        .then(function (reponse){
-            return reponse.json()
-        })
-        .then(function (data){
-            console.log(data)
-        })
+        var tempArray = []
+        // var humidityArray = []
+        // var windArray = []
+        var week = document.getElementById('week')
 
+        fetch(fCast)
+            .then(function (reponse) {
+                return reponse.json()
+            })
+            .then(function (data) {
+                console.log(data)
+
+                // var ftemp = data.list[0].main.temp
+                // var ftempEl = document.createElement('li')
+                // console.log(ftemp)
+
+
+                for (var i = 0; i < data.list.length; i += 8) {
+                    console.log(data.list[i])
+                    var dataList = data.list[i]
+                    var divContainer = document.createElement('div')
+                    divContainer.className = 'container'
+                    var htmlBlock = `
+              <h5>${(dataList.dt_txt).split(' ')[0]}</h5>
+                <img src="https://openweathermap.org/img/wn/${dataList.weather[0].icon}@2x.png" alt="">
+                <p>Temp:${dataList.main.temp}</p>
+                <p>wind:${dataList.wind.speed}</p>
+                <p>Humidity:${dataList.main.humidity}</p>
+              `
+                    divContainer.innerHTML = htmlBlock
+                    week.appendChild(divContainer)
+                    //   tempArray.push(tempArray)
+                    //   console.log(`temp array holds ${tempArray}`)
+                }
+                console.log(tempArray)
+            });
+
+    }
+}
+
+function displayCities(){
+    const historyContainer = document.querySelector('.displayCities')
+    historyContainer.textContent = ''
+    
+    for(var i = 0; i < cities.length; i++){
+        const cityBtn = document.createElement('button')
+        cityBtn.textContent = cities[i]
+        historyContainer.appendChild(cityBtn)
     }
 }
 
 
 
+searchBtn.addEventListener('click', function () {
+    var city = citySearchInput.value.trim()
 
+    cities.push(city)
+    localStorage.setItem('cities', JSON.stringify(cities))
+    
+    displayCities()
 
-searchBtn.addEventListener('click', getApi)
+    getApi(city)
+    
+}) 
+
+displayCities()

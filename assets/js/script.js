986ae6ee3844835a6575b2b04536f8fa503de114
Humberto Alfaro <humbertoalfaro@Humbertos-MBP.lan1>
2023-04-01 17:25:07 -0400

    Updated styling and functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,21 @@ var cities = JSON.parse(localStorage.getItem('cities')) || []
 
 function getApi(city) {
 
-
     if (city === "") {
         return;
     }
 
     var apiUrl = weatherBase + 'weather?' + 'q=' + city + "&units=imperial&appid=" + apiKey
     var cityNameDate = document.getElementById('cityNameDate')
+    cityNameDate.innerHTML = "";
+
     var cityNameDatelEl = document.createElement('h2')
     var today = dayjs()
     cityNameDatelEl.textContent = `${city} ${today.format('M/DD/YYYY')}`;
     cityNameDate.append(cityNameDatelEl)
 
+    tempWindHumidity.innerHTML = "";
+    week.innerHTML = "";
     fetch(apiUrl)
         .then(function (reponse) {
             return reponse.json()
@@ -26,17 +29,14 @@ function getApi(city) {
         .then(function (data) {
             console.log(data)
 
-            // variable for temp
             var temp = document.createElement('li')
             temp.textContent = `Temp: ${data.main.temp} \u00B0F`
             tempWindHumidity.append(temp)
 
-            // variable for humidity
             var humidity = document.createElement('li')
             humidity.textContent = `Humidity: ${data.main.humidity}%`
             tempWindHumidity.append(humidity)
 
-            // variable for wind
             var wind = document.createElement('li')
             wind.textContent = `Wind: ${data.wind.speed} MPH`
             tempWindHumidity.append(wind)
@@ -46,8 +46,7 @@ function getApi(city) {
     function futureForecast() {
         var fCast = weatherBase + 'forecast?' + 'q=' + city + "&units=imperial&appid=" + apiKey
         var tempArray = []
-        // var humidityArray = []
-        // var windArray = []
+
         var week = document.getElementById('week')
 
         fetch(fCast)
@@ -57,10 +56,6 @@ function getApi(city) {
             .then(function (data) {
                 console.log(data)
 
-                // var ftemp = data.list[0].main.temp
-                // var ftempEl = document.createElement('li')
-                // console.log(ftemp)
-
 
                 for (var i = 0; i < data.list.length; i += 8) {
                     console.log(data.list[i])
@@ -69,15 +64,13 @@ function getApi(city) {
                     divContainer.className = 'container'
                     var htmlBlock = `
               <h5>${(dataList.dt_txt).split(' ')[0]}</h5>
-                <img src="https://openweathermap.org/img/wn/${dataList.weather[0].icon}@2x.png" alt="">
+                <img src="https://openweathermap.org/img/wn/${dataList.weather[0].icon}.png" alt="">
                 <p>Temp:${dataList.main.temp}</p>
                 <p>wind:${dataList.wind.speed}</p>
                 <p>Humidity:${dataList.main.humidity}</p>
               `
                     divContainer.innerHTML = htmlBlock
                     week.appendChild(divContainer)
-                    //   tempArray.push(tempArray)
-                    //   console.log(`temp array holds ${tempArray}`)
                 }
                 console.log(tempArray)
             });
@@ -85,16 +78,36 @@ function getApi(city) {
     }
 }
 
-function displayCities(){
-    const historyContainer = document.querySelector('.displayCities')
-    historyContainer.textContent = ''
-    
-    for(var i = 0; i < cities.length; i++){
-        const cityBtn = document.createElement('button')
-        cityBtn.textContent = cities[i]
-        historyContainer.appendChild(cityBtn)
+
+function clearWeather() {
+    tempWindHumidity.innerHTML = "";
+    week.innerHTML = "";
+    cityNameDate.innerHTML = "";
+}
+
+function displayCities() {
+    const historyContainer = document.querySelector(".displayCities");
+    historyContainer.textContent = "";
+
+    const startIndex = Math.max(0, cities.length - 8);
+    const endIndex = cities.length;
+
+    for (var i = startIndex; i < endIndex; i++) {
+        const cityBtn = document.createElement("button");
+        cityBtn.textContent = cities[i];
+        historyContainer.appendChild(cityBtn);
+
+        const city = cities[i];
+        cityBtn.addEventListener("click", function () {
+            return function () {
+                clearWeather();
+                getApi(city);
+            };
+        }());
     }
 }
+
+displayCities();
 
 
 
@@ -103,11 +116,12 @@ searchBtn.addEventListener('click', function () {
 
     cities.push(city)
     localStorage.setItem('cities', JSON.stringify(cities))
-    
+
     displayCities()
 
     getApi(city)
-    
-}) 
+
+})
 
 displayCities()
+
